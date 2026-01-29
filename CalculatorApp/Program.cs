36cf7543da2420f9ff9279eb17a2f6804b2cd535
sainath zinjurde <zinjurdesainath@gmail.com>
2026-01-29 19:49:11 +0530

    become a my frst push requeast

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            double num1, num2, result;
+
+            do
+            {
+                Console.WriteLine("===== SIMPLE CALCULATOR =====");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice >= 1 && choice <= 4)
+                {
+                    Console.Write("Enter first number: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter second number: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            result = num1 + num2;
+                            Console.WriteLine("Result = " + result);
+                            break;
+
+                        case 2:
+                            result = num1 - num2;
+                            Console.WriteLine("Result = " + result);
+                            break;
+
+                        case 3:
+                            result = num1 * num2;
+                            Console.WriteLine("Result = " + result);
+                            break;
+
+                        case 4:
+                            if (num2 != 0)
+                            {
+                                result = num1 / num2;
+                                Console.WriteLine("Result = " + result);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error: Cannot divide by zero");
+                            }
+                            break;
+                    }
+                }
+                else if (choice != 5)
+                {
+                    Console.WriteLine("Invalid choice!");
+                }
+
+                Console.WriteLine();
+            }
+            while (choice != 5);
+
+            Console.WriteLine("Calculator closed. Thank you!");
+        }
+    }
+}
